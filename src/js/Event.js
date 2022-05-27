8a49a90bd2cdb005524d2896f93d6c6675855f13
Sakhildevan <105096037+Sakhildevan@users.noreply.github.com>
2022-05-27 13:17:48 +0530

    file added

--- src/js/Event.js
+++ src/js/Event.js
@@ -11,6 +11,13 @@ function Event() {
                     <h1>Phase 1</h1>
                     <p>October 2021 - December 2021</p>
                 </div>
+                <div className='ii'>
+                <div className='img4'>
+                    <img src={require('../images/event/c (4).png')} alt="" ></img>
+                    </div>
+                    <div className='img5'>
+                    <img src={require('../images/event/t.png')} alt="" ></img></div>
+               </div>
                 <div className='f1'>
                 <div className='tr'>
                 <h3>InCTF Training Round & Team Registration</h3>
@@ -21,37 +28,54 @@ function Event() {
                   <p>Basic CybserSecurity Training, introducing 
                       participants to Web Security & Exploitation, Assembly,
                        Digital Forensics, Reverse Engineering</p>
-                </div></div></div>
+                </div>
+               </div> 
+                </div>
                 <div className='ph2'>
                     <div className='ph'>
                         <h1>Phase 2</h1>
                         <p>December 2021</p>
                     </div>
-                    <div className='f1'>
-                    <div className='tr'>
+                    <div className='iii'>
+                <div className='img6'>
+                    <img src={require('../images/event/q.webp')} alt="" ></img>
+                    </div>
+                    <div className='img7'>
+                    <img src={require('../images/event/c (1).png')} alt="" ></img></div>
+               </div>
+
+                    <div className='f2'>
+                    <div className='tr1'>
                         <h3>Qualifer Round</h3>
                         <p>Participants enhance their skills by practising different
                              CTF-challenges from the Traboda Labs platform.</p>
                     </div>
-                    <div className='bl'>
+                    <div className='bl1'>
                         <h3>Pre - conference talks</h3>
-                        <p>Participants receive advanced talks in different
-                             cybersecurity areas to level up their skills.</p>
+                        <p>Participants receive advanced talks in different cybersecurity areas to level up their skills.</p>
                     </div></div></div>
                     <div className='ph3'>
                         <div className='ph'>
                             <h1>Phase 3</h1>
                             <p> January 2022</p>
-                        </div><div className='f1'>
-                        <div className='tr'>
+                            <div className='iii'>
+                <div className='img8'>
+                    <img src={require('../images/event/c (3).png')} alt="" ></img>
+                    </div>
+                    <div className='img9'>
+                    <img src={require('../images/event/fs.png')} alt="" ></img></div>
+               </div>
+
+                            
+                        </div><div className='f3'>
+                        <div className='tr3'>
                             <h3>InCTF - Finals</h3>
                             <p>Finale of InCTF - Jeopardy 
                                 Style CTF, organized by teambi0s</p>
                                 </div>
-                        <div className='bl'>
+                        <div className='bl3'>
                             <h3>Final Summit</h3>
-                            <p>3-day CyberSecurity Conference, that will
-                                 feature talks, panel-discussions, seminars etc.</p>
+                            <p>3-day CyberSecurity Conference, that will feature talks, panel-discussions, seminars etc.</p>
                         </div>
                     </div>
                 
--- src/js/Why.js
+++ src/js/Why.js
@@ -10,19 +10,19 @@ function Why() {
                 <div className='whh'><h1>Why InCTFCon ?</h1></div>
                      <div className='column1' >
                          <div className='column2'>
-                        <div className='ka km ki'><img src="/images/Data_security_01.310622cf.jpg"></img>
+                        <div className='ka km ki'><img src="/images/Data_security_01.310622cf.jpg" alt=""></img>
                         <h3 className='ke'>There are a lot of untapped opportunities for people to take up the
                              role of safeguarding a digital space.</h3>
                         </div>
-                         <div className='kb km ki'><img src="/images/Data_security_02.52fa4681.jpg"></img>
+                         <div className='kb km ki'><img src="/images/Data_security_02.52fa4681.jpg"alt=""></img>
                          <h3 className='ke'>There is a heavy global demand for cybersecurity professionals but there aren’t many skilled people out there.
                               This job pays really well!</h3>
                          </div>
-                         <div className='kc km ki'><img src="/images/Data_security_03.9797b42a.jpg"></img>
+                         <div className='kc km ki'><img src="/images/Data_security_03.9797b42a.jpg"alt=""></img>
                          <h3  className='ke'>The projected growth of employment in cybersecurity is estimated to be around 
                              3.5 million new job opportunities by 2022.</h3>
                          </div>
-                         <div className='kd km ki'><img src="/images/Data_security_04.220a7ca1.jpg"></img>
+                         <div className='kd km ki'><img src="/images/Data_security_04.220a7ca1.jpg"alt=""></img>
                          <h3  className='ke'>The danger of a cyber pandemic is imminent and the world needs cybersecurity professionals
                               to protect the world from such an attack – just like a superhero!</h3>
                          </div>
